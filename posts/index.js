3160578c1975f03b621cd651eb83aa04ca0e9fad
Snehal Tawade <snehaltawade99@gmail.com>
2024-11-28 07:48:00 +0530

    first commit

---
+++ posts/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import {randomBytes} from 'crypto';
+
+const posts = {}
+const app = express();
+app.use(express.json())
+
+app.get('/posts',(req,res)=>{
+    res.send(posts)
+})
+
+app.post('/posts',(req,res)=>{
+    const id= randomBytes(4).toString('hex')
+    const {title}=req.body
+    posts[id]={
+        id,title
+    }
+    res.status(201).send(posts[id])
+})
+
+app.listen(4000,()=>{
+    console.log("listening on port 4000")
+})
